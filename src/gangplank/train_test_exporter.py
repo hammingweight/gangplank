038a45e197232363f105b21a33854aef3b3db176
Carl Meijer <hammingweight@gmail.com>
2025-05-29 10:37:46 +0200

    Add training metrics.

--- src/gangplank/train_test_exporter.py
+++ src/gangplank/train_test_exporter.py
@@ -4,7 +4,7 @@ except ModuleNotFoundError:
     import tensorflow.keras as keras
 
 import numbers
-from prometheus_client import CollectorRegistry, Gauge, pushadd_to_gateway
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
 
 class TrainTestExporter(keras.callbacks.Callback):
@@ -14,6 +14,7 @@ class TrainTestExporter(keras.callbacks.Callback):
         self.job = job
         self.metrics = metrics
         self.registry = CollectorRegistry()
+        self.gauges = {}
 
     def _get_metrics(self, logs):
         if self.metrics is not None:
@@ -25,10 +26,23 @@ class TrainTestExporter(keras.callbacks.Callback):
 
         return metrics
 
+    def _get_gauge(self, name, desc):
+        if not self.gauges.get(name):
+            self.gauges[name] = Gauge(name, desc, registry=self.registry)
+        return self.gauges[name]
+
     def on_test_end(self, logs):
         metrics = self._get_metrics(logs)
         for k in metrics:
-            gauge = Gauge("gangplank_" + k, k, registry=self.registry)
+            gauge = self._get_gauge("gangplank_test_" + k, k)
             gauge.set(logs[k])
 
-        pushadd_to_gateway(self.pgw_addr, self.job, self.registry)
+        push_to_gateway(self.pgw_addr, self.job, self.registry)
+
+    def on_epoch_end(self, epoch, logs):
+        metrics = self._get_metrics(logs)
+        for k in metrics:
+            gauge = self._get_gauge("gangplank_train_" + k, k)
+            gauge.set(logs[k])
+
+        push_to_gateway(self.pgw_addr, self.job, self.registry)
