b5c108f74d9cc81463a07af073f299fedfd0d6a1
Carl Meijer <hammingweight@gmail.com>
2025-06-02 13:48:59 +0200

    Fix comment.

--- src/gangplank/__init__.py
+++ src/gangplank/__init__.py
@@ -5,8 +5,9 @@ which can be found at gangplank's PyPI URL: https://pypi.org/project/gangplank/
 
 from .train_test_exporter import (
     TrainTestExporter,
-    # Represents histogram weight buckets for range 0 to 3.
+    # Represents histogram weight buckets in interval [-0.3, +0.3]
     HISTOGRAM_WEIGHT_BUCKETS_0_3,
+    # Represents histogram weight buckets in interval [-1.0, +1.0]
     HISTOGRAM_WEIGHT_BUCKETS_1_0,
 )
 
