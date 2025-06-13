7efa739560ce12e33ecf8f21a49bb5e8ab9a4397
Carl Meijer <hammingweight@gmail.com>
2025-06-13 16:55:31 +0200

    Move test.

--- examples/drift/chi_square/no_drift.py
+++ examples/drift/chi_square/no_drift.py
@@ -15,37 +15,10 @@ train_images = train_images.astype("float32") / 255
 test_images = test_images.reshape((10000, 28, 28, 1))
 test_images = test_images.astype("float32") / 255
 
-# Get expected predictions for the first 20000 images in the training model
-preds = model.predict(train_images[:20000], verbose=0)
-
-# Create an online drift detector (MMD)
-drift_detector = cd.MMDDriftOnline(
-    preds, ert=500, window_size=100, backend="pytorch", n_bootstraps=5000
-)
-
-
-# A closure that uses the MMD to check whether predicted values are drifting
-# from the training data. The function returns the number of times that
-# the MMD reports that drift was detected in the predictions. Note
-# that the input values (_X) are discarded since we're interested in
-# prediction drift not data drift.
-def get_drift_metrics(_X, Y):
-    count = 0
-    value = None
-    for y in Y:
-        res = drift_detector.predict(y, return_test_stat=True)["data"]
-        if res["is_drift"] == 1:
-            count += 1
-        # Uncomment the next line if you want to publish MMD values
-        # as metrics.
-        # v = res["test_stat"]
-    return (count, value)
-
-
 # A proxy that instruments the Keras model. We use the closure to check for
 # prediction drift so that we can emit drift metrics.
 model = gangplank.PrometheusModel(
-    model, port=8561, get_drift_metrics_func=get_drift_metrics
+    model, port=8561
 )
 
 input("Press Enter to continue...")
@@ -53,7 +26,7 @@ input("Press Enter to continue...")
 # Run some predictions without inducing drift
 print("Predictions without drift...")
 start_time = time.time()
-while time.time() - start_time < 300:
+while time.time() - start_time < 900:
     idx = random.randint(0, 9999)
     model.predict(test_images[idx].reshape(1, 28, 28, 1), verbose=0)
 
