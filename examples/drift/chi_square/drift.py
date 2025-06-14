3741317659e46ea83419c1d0890c0cb01e88f69e
Carl Meijer <hammingweight@gmail.com>
2025-06-14 16:29:52 +0200

    More alerting.

--- examples/drift/chi_square/drift.py
+++ examples/drift/chi_square/drift.py
@@ -15,20 +15,22 @@ model = keras.models.load_model("../../models/mnist_convnet.keras")
 test_images = test_images.reshape((10000, 28, 28, 1))
 test_images = test_images.astype("float32") / 255
 
+
 def get_drift_metrics(_X, Y):
-    #print(len(Y))
     if len(Y) < 50:
         return gangplank.Drift()
     buckets = [0] * 10
     for y in Y:
         buckets[y.argmax()] += 1
-    #print(buckets)
-    #stats.chisquare(buckets)
-    return gangplank.Drift(p_value=stats.chisquare(buckets).pvalue)
+    res = stats.chisquare(buckets)
+    return gangplank.Drift(p_value=res.pvalue, test_statistic=res.statistic)
+
 
 # A proxy that instruments the Keras model. We use the closure to check for
 # prediction drift so that we can emit drift metrics.
-model = gangplank.PrometheusModel(model, port=8561, get_drift_metrics_func=get_drift_metrics)
+model = gangplank.PrometheusModel(
+    model, port=8561, get_drift_metrics_func=get_drift_metrics
+)
 
 input("Press Enter to continue...")
 
@@ -37,7 +39,7 @@ print("Predictions without drift...")
 start_time = time.time()
 while time.time() - start_time < 900:
     batch = []
-    for _ in range(0, random.randint(1,200)):
+    for _ in range(0, random.randint(1, 200)):
         idx = random.randint(0, 9999)
         batch.append(test_images[idx])
     batch = np.array(batch)
@@ -45,8 +47,7 @@ while time.time() - start_time < 900:
 
 # Artificially induce drift by discarding all images of zero.
 print("Predictions with drift...")
-start_time = time.time()
-while time.time() - start_time < 900:
+while True:
     batch = []
     size = random.randint(1, 200)
     i = 0
