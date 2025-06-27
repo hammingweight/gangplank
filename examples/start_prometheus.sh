1b6b70a96487eea2ee59f0d90a7c3299bb9ec7b6
Carl Meijer <hammingweight@gmail.com>
2025-06-27 06:04:15 +0200

    Escape paths.

--- examples/start_prometheus.sh
+++ examples/start_prometheus.sh
@@ -6,9 +6,9 @@ if command -v "realpath" &>/dev/null; then
     SCRIPT_DIR=$(realpath "$SCRIPT_DIR")
 fi
 
-docker run --rm --name prometheus -v $SCRIPT_DIR/prometheus:/etc/prometheus/ -d --network host prom/prometheus
+docker run --rm --name prometheus -v "$SCRIPT_DIR/prometheus:/etc/prometheus/" -d --network host prom/prometheus
 docker run --rm --name pgw -d --network host prom/pushgateway
-docker run --rm --name alertmanager -v $SCRIPT_DIR/alertmanager:/etc/alertmanager -d --network host prom/alertmanager
+docker run --rm --name alertmanager -v "$SCRIPT_DIR/alertmanager:/etc/alertmanager" -d --network host prom/alertmanager
 
 # Check that prometheus and pgw didn't crash
 sleep 10
